3b36fd3de9ac0c1f5471e72a844201e5f65d4fb1
agengw10 <agengw10@gmail.com>
2019-06-17 15:48:28 +0700

    update header

--- src/header.js
+++ src/header.js
@@ -13,7 +13,7 @@ class HeaDer extends Component {
     render() {
         const {HeaDer1,HeaDer2,HeaDer3,HeaDer4}=this.props;        
         return (
-            <div className="container">
+            <div className="">
                 <div className="row">
                     <div className="col-md-12 col-xs-12 logo">   
                         <a className="navbar-brand linkcolor"><img src={logo} width="100" className="atas" alt="..."></img></a>
